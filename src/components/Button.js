a66c8dba1515858b97a6707c8b5ee9aaab96ff4b
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-15 14:29:53 +0300

    refactor Button to use function component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+export default function Button(props) {
+  const { value, onClick } = props;
 
-  render() {
-    const { value, onClick } = this.props;
-    let className = /[0-9.]/.test(value) ? 'btn' : 'btn btn-operation';
-    className += value === '0' ? ' btn-0' : '';
+  let className = /[0-9.]/.test(value) ? 'btn' : 'btn btn-operation';
+  className += value === '0' ? ' btn-0' : '';
 
-    return (
-      <button
-        className={className}
-        onClick={() => onClick(value)}
-        type="button"
-      >
-        {value}
-      </button>
-    );
-  }
+  return (
+    <button
+      className={className}
+      onClick={() => onClick(value)}
+      type="button"
+    >
+      {value}
+    </button>
+  );
 }
 
 Button.propTypes = {
