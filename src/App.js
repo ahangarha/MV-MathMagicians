e217219dabd50c578cfa945a3aa23de05b76ba6c
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-15 14:38:11 +0300

    refactor App to use function component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Calculator />
-    );
-  }
+export default function App() {
+  return (
+    <Calculator />
+  );
 }
