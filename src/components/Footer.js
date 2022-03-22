51f4a196db473fe317a79ed3258dd767240effd5
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-22 12:48:24 +0300

    add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      This is a Free/Libre Software under
+      <a
+        href="https://github.com/ahangarha/MV-MathMagicians/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        AGPL license
+      </a>
+      .
+    </footer>
+  );
+}
