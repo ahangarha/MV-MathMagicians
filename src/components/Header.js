6cde8ae6af7535f93f63b2e2b679fde0f75c60f0
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-22 15:08:19 +0300

    improve responsiveness for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,24 +5,26 @@ import './Header.css';
 export default function Header() {
   return (
     <header>
-      <h1 className="branding">
-        Math
-        <br />
-        Magicians
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/calculator">Calculator</Link>
-          </li>
-          <li>
-            <Link to="/quote">Quote</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="container">
+        <h1 className="branding">
+          Math
+          <br />
+          Magicians
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calculator">Calculator</Link>
+            </li>
+            <li>
+              <Link to="/quote">Quote</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
