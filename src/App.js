f10bc1ccafecf694148641dc13224f134d246241
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-22 12:49:10 +0300

    make general layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
-    <Calculator />
+    <>
+      <Header />
+      <main id="tabs">
+        <Calculator />
+      </main>
+      <Footer />
+    </>
   );
 }
