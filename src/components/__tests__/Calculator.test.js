d200244beee99b97e9dc3db3c0103b655ae6c34e
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-24 17:06:39 +0300

    add e2e test for calculator plus and minus operation

--- src/components/__tests__/Calculator.test.js
+++ src/components/__tests__/Calculator.test.js
@@ -1,3 +1,4 @@
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import Renderer from 'react-test-renderer';
 import Calculator from '../Calculator';
@@ -8,4 +9,28 @@ it('Renders correctly', () => {
     .toJSON();
 
   expect(tree).toMatchSnapshot();
+});
+
+it('shows 14 on "5 + 9 =" operation', () => {
+  render(<Calculator />);
+
+  fireEvent.click(screen.getByRole('button', { name: '5' }));
+  fireEvent.click(screen.getByRole('button', { name: '+' }));
+  fireEvent.click(screen.getByRole('button', { name: '9' }));
+  fireEvent.click(screen.getByRole('button', { name: '=' }));
+
+  expect(screen.getByText('14')).toBeTruthy();
+});
+
+it('shows 552 on "555 - 3 =" operation', () => {
+  render(<Calculator />);
+
+  fireEvent.click(screen.getByRole('button', { name: '5' }));
+  fireEvent.click(screen.getByRole('button', { name: '5' }));
+  fireEvent.click(screen.getByRole('button', { name: '5' }));
+  fireEvent.click(screen.getByRole('button', { name: '-' }));
+  fireEvent.click(screen.getByRole('button', { name: '3' }));
+  fireEvent.click(screen.getByRole('button', { name: '=' }));
+
+  expect(screen.getByText('552')).toBeTruthy();
 });
